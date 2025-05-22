f9b7064ae8efa1f47c4b1f210d5bb09ec658513e
bi4nki <70487195+bi4nki@users.noreply.github.com>
2025-05-22 18:33:34 -0300

    Create dashboardRoutes.js

---
+++ backend-api/routes/dashboardRoutes.js
@@ -0,0 +1,9 @@
+// backend-api/routes/dashboardRoutes.js
+import express from 'express';
+import { getDashboardStats } from '../controllers/dashboardController.js';
+
+const router = express.Router();
+
+router.get('/stats', getDashboardStats);
+
+export default router;
