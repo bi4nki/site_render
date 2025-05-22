f8e7bf2995ceb29d3ccb1d7484e41cf9240e9993
bi4nki <70487195+bi4nki@users.noreply.github.com>
2025-05-22 20:19:31 -0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Outras configurações do Next.js podem ir aqui
 };
 
-export default nextConfig; // Ou module.exports = nextConfig; se for .js
+export default nextConfig;
