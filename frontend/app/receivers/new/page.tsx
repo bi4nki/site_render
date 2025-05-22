bafae19ee27a9954cf0506657460a6772b0ede5f
bi4nki <70487195+bi4nki@users.noreply.github.com>
2025-05-22 20:16:54 -0300

    Update page.tsx

--- frontend/app/receivers/new/page.tsx
+++ frontend/app/receivers/new/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import ReceiverForm, { ReceiverFormData } from '../../components/ReceiverForm'; // Ajuste o path
+import ReceiverForm, { ReceiverFormData } from '../../components/ReceiverForm';
 
 export default function NewReceiverPage() {
   const [isSubmitting, setIsSubmitting] = useState(false);
