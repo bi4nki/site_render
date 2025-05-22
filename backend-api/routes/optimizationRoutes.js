d37f326017a7f96659c8997b74a77d2aef22f1e4
bi4nki <70487195+bi4nki@users.noreply.github.com>
2025-05-22 20:02:52 -0300

    Update optimizationRoutes.js

--- backend-api/routes/optimizationRoutes.js
+++ backend-api/routes/optimizationRoutes.js
@@ -1,11 +1,9 @@
 
 import express from 'express';
-import { optimizeTransport } from '../controllers/optimizationController.js'; // Criaremos este controller a seguir
+import { optimizeTransport } from '../controllers/optimizationController.js';
 
 const router = express.Router();
 
-// Rota para iniciar o processo de otimização de transporte
-// Espera { donorId: number, receiverId: number } no corpo da requisição
 router.post('/optimize', optimizeTransport);
 
 export default router;
